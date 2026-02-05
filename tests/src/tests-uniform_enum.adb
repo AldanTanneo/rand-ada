f3f486ae18bfb154174520a18d8acaa92dca0cba
AldanTanneo <aldantanneo@gmail.com>
2026-02-05 15:45:21 +0100

    test enumeration sampling

---
+++ tests/src/tests-uniform_enum.adb
@@ -0,0 +1,18 @@
+with Rand; use Rand;
+with Rand_Distributions.Uniform;
+
+procedure Tests.Uniform_Enum is
+   type T is (A, B, C, D, E, F);
+
+   package T_Distr is new Rand_Distributions.Generic_Distribution (T);
+   package T_Uniform is new Rand_Distributions.Uniform.Discrete (T, T_Distr);
+
+   R : Rng'Class := Thread_Rng;
+   U1 : constant T_Uniform.Distribution := T_Uniform.Create;
+   U2 : constant T_Uniform.Distribution := T_Uniform.Create (B, E);
+begin
+   for I in 1 .. 10000 loop
+      Assert (U1.Sample (R) in T);
+      Assert (U2.Sample (R) in B .. E);
+   end loop;
+end Tests.Uniform_Enum;
