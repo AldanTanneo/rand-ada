2b545ac795a0c90dff2726fae7ca27c623c8a3b5
AldanTanneo <aldantanneo@gmail.com>
2026-02-09 19:14:09 +0100

    fix standard floating point generation

--- rand_core/src/rand_core-generators.adb
+++ rand_core/src/rand_core-generators.adb
@@ -5,14 +5,34 @@ with Rand_Core.Utils;
 package body Rand_Core.Generators
   with Pure
 is
-   function Gen (R : in out Rng'Class) return Float
-   is (Float'Compose
-         (Float (Utils.Shr (R.Next, 64 - Float'Machine_Mantissa)), 1)
-       - 1.0);
+   function Generic_Float (R : in out Rng'Class) return F is
+      use type U64;
+
+      Bits_To_Discard : constant Natural := 64 - F'Machine_Mantissa;
+      Msb_Mask        : constant U64 := 2 ** (F'Machine_Mantissa - 1);
+
+      Mantissa : constant U64 :=
+        Utils.Shr (R.Next, Bits_To_Discard) or Msb_Mask;
+      --  shift out the non significant bits + 1, and add a `1` MSB to get
+      --  a mantissa that is always the same size
+
+      Value_1_2 : constant F := F'Compose (F (Mantissa), 1);
+      pragma Assert (Value_1_2 >= 1.0 and then Value_1_2 < 2.0);
+      --  set the exponent to 1 in the canonical representation, getting a
+      --  value uniformly sampled from [1, 2)
+      Value_0_1 : constant F := Value_1_2 - 1.0;
+      pragma Assert (Value_0_1 >= 0.0 and then Value_0_1 < 1.0);
+      --  substract 1 to get a value in [0, 1)
+   begin
+      return Value_0_1;
+   end Generic_Float;
+
+   function Gen_Float is new Generic_Float (Float);
+   function Gen_Long_Float is new Generic_Float (Long_Float);
+
+   function Gen (R : in out Rng'Class) return Float renames Gen_Float;
    function Gen (R : in out Rng'Class) return Long_Float
-   is (Long_Float'Compose
-         (Long_Float (Utils.Shr (R.Next, 64 - Long_Float'Machine_Mantissa)), 1)
-       - 1.0);
+   renames Gen_Long_Float;
 
    function U32_To_Int is new Ada.Unchecked_Conversion (U32, Integer);
    function U64_To_Int is new Ada.Unchecked_Conversion (U64, Long_Integer);
--- rand_core/src/rand_core-generators.ads
+++ rand_core/src/rand_core-generators.ads
@@ -2,18 +2,39 @@ package Rand_Core.Generators
   with Pure
 is
    type Rng is limited interface;
+   --  Interface for a random number generator
 
    function Next (R : in out Rng) return U64 is abstract;
+   --  Update the generator state and output the next 64-bit value
    procedure Next_Bytes (R : in out Rng; Buf : out Bytes) is abstract;
+   --  Update the generator state and fill the provided buffer with random
+   --  bytes
+
+   --
+   --  utility functions for generating standard types
+   --
+
+   generic
+      type F is digits <>;
+      pragma
+        Compile_Time_Error
+          (F'Machine_Mantissa > 64, "type mantissa must be <=64");
+   function Generic_Float (R : in out Rng'Class) return F
+   with Inline;
+   --  Generic function to generate a floating point value in the range [0, 1)
 
    function Gen (R : in out Rng'Class) return Float
    with Inline_Always;
+   --  Return a Float in [0, 1)
    function Gen (R : in out Rng'Class) return Long_Float
    with Inline_Always;
+   --  Return a Long_Float in [0, 1)
    function Gen (R : in out Rng'Class) return Integer
    with Inline_Always;
+   --  Return a random Integer over the whole range
    function Gen (R : in out Rng'Class) return Long_Integer
    with Inline_Always;
+   --  Return a random Long_Integer over the whole range
 
    function Gen (R : in out Rng'Class) return I8
    with Inline_Always;
