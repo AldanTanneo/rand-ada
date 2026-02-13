3517b32f4ad22538b35e6954ec9b4301ae0f6ee7
AldanTanneo <aldantanneo@gmail.com>
2026-02-13 16:24:56 +0100

    add basic distribution checks to tests

--- tests/src/tests-uniform_enum.adb
+++ tests/src/tests-uniform_enum.adb
@@ -2,6 +2,9 @@ with Rand; use Rand;
 with Rand_Distributions.Uniform;
 
 procedure Tests.Uniform_Enum is
+   N       : constant := 1000000;
+   Epsilon : constant := 1500;
+
    type T is (A, B, C, D, E, F);
 
    package T_Distr is new Rand_Distributions.Generic_Distribution (T);
@@ -10,9 +13,29 @@ procedure Tests.Uniform_Enum is
    R  : Rng := Thread_Rng;
    U1 : constant T_Uniform.Distribution := T_Uniform.Create;
    U2 : constant T_Uniform.Distribution := T_Uniform.Create (B, E);
+
+   X      : T;
+   C1, C2 : array (T) of Long_Integer := [others => 0];
+
+   Est1 : constant Long_Integer := N / 6;
+   Est2 : constant Long_Integer := N / 4;
 begin
-   for I in 1 .. 1000000 loop
-      Assert (U1.Sample (R) in T, "invalid enum member");
-      Assert (U2.Sample (R) in B .. E, "enum member not in required range");
+   for I in 1 .. N loop
+      X := U1.Sample (R);
+      Assert (X'Valid, "invalid enum member");
+      C1 (X) := C1 (X) + 1;
+
+      X := U2.Sample (R);
+      Assert (X in B .. E, "enum member not in required range");
+      C2 (X) := C2 (X) + 1;
    end loop;
+
+   Assert
+     ((for all I in A .. F => abs (C1 (I) - Est1) < Epsilon),
+      "counts too biased for #1: " & C1'Image & ", expected" & Est1'Image);
+
+   Assert
+     ((for all I in B .. E => abs (C2 (I) - Est2) < Epsilon),
+      "counts too biased for #2: " & C2'Image & ", expected" & Est2'Image);
+   Assert (C2 (A) = 0 and then C2 (F) = 0);
 end Tests.Uniform_Enum;
--- tests/src/tests-uniform_floats.adb
+++ tests/src/tests-uniform_floats.adb
@@ -1,27 +1,80 @@
+with Ada.Numerics.Long_Elementary_Functions;
+
 with Rand; use Rand;
 
 procedure Tests.Uniform_Floats is
+   N       : constant := 10000000;
+   Epsilon : constant := 1.0;
+
    R : Rng := Thread_Rng;
 
-   X  : Float;
-   subtype U1 is Distributions.Interfaces.Float_Distr.Distribution'Class;
-   D1 : constant U1 := Distributions.Uniform_Float.Create (50.0, 2500.0);
-   D2 : constant U1 := Distributions.Uniform_Float.Create (-1000.0, 1000.0);
+   X, M1, M2, V1, V2 : Long_Float := 0.0;
+
+   subtype D is Distributions.Interfaces.Long_Float_Distr.Distribution'Class;
+   D1 : constant D := Distributions.Uniform_Long_Float.Create (50.0, 2500.0);
+   D2 : constant D :=
+     Distributions.Uniform_Long_Float.Create (-1000.0, 1000.0);
+
+   Mean1 : constant Long_Float := (2500.0 + 50.0) / 2.0;
+   Var1  : constant Long_Float :=
+     Ada.Numerics.Long_Elementary_Functions.Sqrt ((2500.0 - 50.0) ** 2 / 12.0);
+   Mean2 : constant Long_Float := 0.0;
+   Var2  : constant Long_Float :=
+     Ada.Numerics.Long_Elementary_Functions.Sqrt (2000.0 ** 2 / 12.0);
 
    Y  : Long_Float;
-   subtype U2 is Distributions.Interfaces.Long_Float_Distr.Distribution'Class;
-   D3 : constant U2 :=
+   D3 : constant D :=
      Distributions.Uniform_Long_Float.Create
        (Long_Float'First / 2.0, Long_Float'Last / 2.0);
 begin
-   for I in 1 .. 1000000 loop
+   for I in 1 .. N loop
       X := D1.Sample (R);
       Assert (50.0 <= X and then X < 2500.0, "float not in required range");
+      M1 := M1 + (X - Mean1);
+      V1 := V1 + (X - Mean1) ** 2;
 
       X := D2.Sample (R);
       Assert (-1000.0 <= X and then X < 1000.0, "float not in required range");
+      M2 := M2 + (X - Mean2);
+      V2 := V2 + (X - Mean2) ** 2;
 
       Y := D3.Sample (R);
       Assert (Y'Valid, "invalid Long_Float");
    end loop;
+
+   M1 := M1 / Long_Float (N);
+   V1 :=
+     Ada.Numerics.Long_Elementary_Functions.Sqrt
+       (V1 / Long_Float (N) - M1 ** 2);
+
+   Assert
+     (abs M1 < Epsilon,
+      "mean #1 too far from wanted value:"
+      & Long_Float'(M1 + Mean1)'Image
+      & ", expected"
+      & Mean1'Image);
+   Assert
+     (abs (V1 - Var1) < Epsilon,
+      "variance #1 too far from wanted value:"
+      & V1'Image
+      & ", expected"
+      & Var1'Image);
+
+   M2 := M2 / Long_Float (N);
+   V2 :=
+     Ada.Numerics.Long_Elementary_Functions.Sqrt
+       (V2 / Long_Float (N) - M2 ** 2);
+
+   Assert
+     (abs M2 < Epsilon,
+      "mean #2 too far from wanted value:"
+      & Long_Float'(M2 + Mean2)'Image
+      & ", expected"
+      & Mean2'Image);
+   Assert
+     (abs (V2 - Var2) < Epsilon,
+      "variance #2 too far from wanted value:"
+      & V2'Image
+      & ", expected"
+      & Var2'Image);
 end Tests.Uniform_Floats;
